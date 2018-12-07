4f21471e7df2d237378e320774f4b9a0e4136a34
Jonathan McDonald <Jonathan@Jonathan.local>
2018-12-07 10:55:40 -0700

    completed backend CRUD

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -1,7 +1,53 @@
-tasks: [];
+let practiceChart = [
+    {
+        id: 1,
+        composer: 'Kapustin',
+        piece: 'Pastorale',
+        tempo: 180,
+        notes: 'Fix articulation'
+    },
+    {
+        id: 2,
+        composer: 'Liszt',
+        piece: 'Etude',
+        tempo: 136,
+        notes: 'Get tempo to 136'
+    }
+];
+
+let taskID = 3;
 
 module.exports = {
-    getAll: (req, res) => {
-        res.status(200).send()
+    fetchPracticeChart: (req, res) => {
+        res.status(200).send(practiceChart)
+    },
+    newPracticeItem: (req, res) => {
+        practiceChart.push({
+            id: taskID,
+            composer: req.body.composer,
+            piece: req.body.piece,
+            tempo: req.body.tempo,
+            notes: req.body.notes
+        })
+        taskID++
+        res.status(200).send(practiceChart)
+    },
+    updatePracticeItem: (req, res) => {
+        let itemIndex = practiceChart.findIndex(item => item.id === +req.params.id)
+        practiceChart[itemIndex] = {
+            id: req.params.id,
+            composer: req.body.composer,
+            piece: req.body.piece,
+            tempo: req.body.tempo,
+            notes: req.body.notes
+        }
+        res.status(200).send(practiceChart)
+    },
+    deleteItem: (req, res) => {
+        let itemIndex = practiceChart.findIndex(item => item.id === +req.params.id)
+        console.log(itemIndex)
+        practiceChart.splice(itemIndex, 1)
+        res.status(200).send(practiceChart)
     }
+
 }
--- server/index.js
+++ server/index.js
@@ -4,10 +4,16 @@ const controller = require('./controllers/mainController.js')
 const app = express()
 
 // Top Level Middleware:
-app.us(expres.json())
+app.use(express.json())
 
-// End points:
-app.get(`/api/poiu`, controller.getAll)
+// // End points:
+app.get(`/api/chart`, controller.fetchPracticeChart)
+
+app.post(`/api/chart`, controller.newPracticeItem)
+
+app.put(`/api/chart/:id`, controller.updatePracticeItem)
+
+app.delete(`/api/chart/:id`, controller.deleteItem)
 
 const PORT = 4040
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
